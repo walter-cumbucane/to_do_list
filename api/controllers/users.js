e36ef4568aa52aaebba19088c58d8fc650c5afe6
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-15 16:35:35 +0200

    CORS handled. Nodemon added

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,9 +1,8 @@
 
 
-
 exports.get_all = async (req, res, next) => {
 
     res.status(200).json({
-        message: 'GET ROUTE properly working'
+        message: 'GET ROUTE working'
     });
 }
--- app.js
+++ app.js
@@ -2,14 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const usersRouter = require('./api/routes/users');
 const tasksRouter = require('./api/routes/tasks');
-
 const app = express();
 
+
+//Handles Json's requests parsing
 app.use(bodyParser.json());
+
+
+//Handles CORS
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',  'Origin, X-Requested-With, Content-Type, Authorization');
+    
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, DELETE, POST, PUT');
+        res.status(200).json({});
+    } 
+    next();
+});
+
+
+
+//Handles routing
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/tasks', tasksRouter);
 
-const PORT = 5000 || process.env.PORT;
+
+
+//Server Listening Setup
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
